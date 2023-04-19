4f0002d7b4dda3d45d26a87547ab18629ce78c62
Pedro <50713178+prgf87@users.noreply.github.com>
2023-04-19 18:15:00 +0100

    1.2

--- components/modules/Resume.tsx
+++ components/modules/Resume.tsx
@@ -43,28 +43,34 @@ function Resume({ experiences }: Props) {
           <div className="grid p-10 mt-40 gap-y-20">
             {experiences.map((experience) => {
               return (
-                <article
-                  key={experience._id}
-                  className="w-full flex flex-row rounded-lg items-left bg-slate-100/90 opacity-70 hover:opacity-100 cursor-pointer transition-opacity duration-200 text-gray-800"
-                >
-                  <div className="px-0 md:px-10">
-                    <h1 className="text-3xl font-bold text-left pt-10">
-                      {experience?.jobtitle}
-                    </h1>
-                    <h2 className="text-xl font-bold mt-1 text-left">
-                      {experience?.company}
-                    </h2>
-                    <p className="uppercase py-5 text-slate-900 text-left">
-                      Started -{' '}
-                      {new Date(experience.dateStarted).toDateString()}{' '}
-                      <br></br>
-                      Ended - {new Date(experience.dateEnded).toDateString()}
-                    </p>
-                    <div>
-                      <PortableText
-                        value={experience.body}
-                        components={RichTextComponents}
-                      />
+                <article key={experience._id} className="w-full">
+                  <div className="grid grid-cols-3 mx-auto max-w-7xl">
+                    <div className="flex justify-center items-center text-xl font-bold">
+                      <div>
+                        <h2 className="text-xl font-bold mt-1 text-left uppercase">
+                          {experience?.company}
+                        </h2>
+                        <p className="text-xl font-bold py-5 text-gray-200 text-left">
+                          Started -{' '}
+                          {new Date(experience.dateStarted).toDateString()}{' '}
+                          <br></br>
+                          Ended -{' '}
+                          {new Date(experience.dateEnded).toDateString()}
+                        </p>
+                      </div>
+                    </div>
+
+                    <div className="col-span-2 px-0 md:px-10 rounded-lg items-left bg-slate-100/90 text-gray-800">
+                      <h1 className="text-3xl font-bold text-left pt-10">
+                        {experience?.jobtitle}
+                      </h1>
+
+                      <div>
+                        <PortableText
+                          value={experience.body}
+                          components={RichTextComponents}
+                        />
+                      </div>
                     </div>
                   </div>
                 </article>
